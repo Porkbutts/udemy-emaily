22a39b4f775f84cf3afaa7e5c43463df850b7933
Adrian Teng-Amnuay <pumpadrian@gmail.com>
2021-07-20 20:45:19 -0700

    fix lints and add request to prod

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' })
-})
+  res.send({ bye: 'buddy' });
+});
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
